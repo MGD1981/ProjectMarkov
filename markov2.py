7a5e801b4085ed06bb2ee74c55213607305df947
gremdavel <MGD1981@gmail.com>
2013-06-19 13:41:15 -0400

    Created new method of generating Markov chain in markov2.py, based on creating a dictionary.  Also modified projectmarkov.py to be able to take a local file rather than just pulling a website.

---
+++ markov2.py
@@ -0,0 +1,53 @@
+from sys import argv
+from random import choice
+
+script, file1 = argv
+
+def genchain(dictsource, length):
+    """
+    Returns a string 'length' words long, based on dictionary 'dictsource'
+    and generated with a Markov chain algorithm."""
+    print "\nGenerating Markov chain...\n"
+    newtext = []
+    word = choice(dictsource.keys())
+
+    while length > 0:
+        word = choice(dictsource[word])
+        newtext.append(word)
+        length -= 1
+
+    return " ".join(newtext)
+    
+
+def createlib(text):
+    """
+    Takes text file and creates a library, linking each word in the files
+    to the word that comes after it, and the number of times that sequence
+    occurs."""
+    print "\nCreating associative library of words..."
+    worddict = {}
+    word = '\n'
+    text = open(text).read().split(" ")
+
+    for x in text:
+        worddict.setdefault(word, []).append(x)
+        oldword = word
+        word = x
+   
+    return worddict 
+
+
+
+print "\nHow many words would you like to generate?"
+try:
+    w = int(raw_input(">> "))
+except ValueError:
+    w = 500
+if w <= 0:
+    w = 500
+
+worddict = createlib(file1)
+newtext = genchain(worddict, w)
+
+print newtext
+
--- projectmarkov.py
+++ projectmarkov.py
@@ -1,22 +1,30 @@
-import pdb
+from sys import argv
 import urllib
 import htmlslicer
 import markov
 
+if len(argv) > 1:
+    script, file1 = argv
 
-print "\nPlease enter a URL:"
-u = raw_input(">> ")
-if len(u) < 1:
-    u = 'http://www.thefatalistmarksman.com/'
+if len(argv) < 2:
+    print "\nPlease enter a URL:"
+    u = raw_input(">> ")
+    if len(u) < 1:
+        u = 'http://www.thefatalistmarksman.com/'
 print "\nHow many words would you like to generate?"
 try:
     w = int(raw_input(">> "))
 except ValueError:
-    w = None
+    w = 500
 if w <= 0:
-    w = None
-print "\nPlease be patient as we retrieve web content...\n"
-f = urllib.urlopen(u, 'r')
+    w = 500
+
+if len(argv) < 2: 
+    print "\nPlease be patient as we retrieve web content...\n"
+    f = urllib.urlopen(u, 'r')
+else:
+    f = open(file1, 'r')
+
 s = f.read()
 f.close()
 
